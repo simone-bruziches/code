d233697ed4783cc1af23ca7182f112f4bdf5080d
Simone bruziches <simone.shaggyowl@gmail.com>
2019-10-18 16:42:53 +0200

    DS Store eliminato

---
+++ code/palestreview.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html>
+
+<div > 
+<?php foreach($loc as $lc){
+ echo $lc;
+ ?>
+</div>
+
+
+</html>
+
+//UPDATE PALESTRE
