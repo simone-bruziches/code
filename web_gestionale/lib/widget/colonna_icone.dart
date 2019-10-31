9109b1faaaf08e059e1fff014bb16f6806df022b
shaggypeps <shaggy.owl.team@gmail.com>
2019-10-31 18:21:00 +0100

    Modificato nome WOD nel menu , era sbagliato

--- web_gestionale/lib/widget/colonna_icone.dart
+++ web_gestionale/lib/widget/colonna_icone.dart
@@ -29,7 +29,7 @@ class _ColonnaIconeState extends State<ColonnaIcone>
     'Informazioni palestra',
     'Clienti',
     'Abbonamenti',
-    'Wuod',
+    'WOD',
     'Palinsesto',
     'Prenotazioni',
     'Agenda',
